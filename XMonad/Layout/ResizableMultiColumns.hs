fd9efef2ba194e8921d8764a2379f97f1e87993c
4caraml <86114387+4caraml@users.noreply.github.com>
2021-06-24 09:56:41 +0200

    rewrite w/ vertical resizing

--- XMonad/Layout/ResizableMultiColumns.hs
+++ XMonad/Layout/ResizableMultiColumns.hs
@@ -1,130 +1,67 @@
-{-# LANGUAGE ExistentialQuantification, FlexibleContexts, FlexibleInstances,
-             LambdaCase, MultiParamTypeClasses, MultiWayIf,
-             NoMonomorphismRestriction, RecordWildCards, TupleSections #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module XMonad.Layout.ResizableMultiColumns
-  ( ncolumns
-  , DMsg(..), ModNCol(..)
+  ( IncColN(..)
+  , ResizeAt(..)
+  , RMC(..), rmc
   ) where
 
-import           Data.Functor
-import           Data.List
 import           Control.Monad
 import           Data.Ratio
 import           XMonad
+import           XMonad.Layout.ResizableTile (MirrorResize(..))
 import qualified XMonad.StackSet as W
 import           XMonad.Util.Types
 
 
--- TODO: remove
-notify :: Show a => a -> X ()
-notify x = spawn $ "notify-send '" ++ show x ++ "'"
+type Grid m n = [(m,[n])]
 
--- | invariants that should hold @length nums == length widths@,
--- @0 < length nums@ and @sum widths == 1@
-data NColumns a = NColumns
-  { nums   :: ![Int]
-  , widths :: ![Ratio Int]
-  , delta  :: !(Ratio Int)
+data RMC a = RMC
+  { cdef  :: !Rational
+  , rdef  :: ![Rational]
+  , delta :: !Rational
+  , keep  :: !Int
+  , grid  :: !(Grid Rational Rational)
   } deriving (Show, Read)
 
-ncolumns = NColumns [1] [1] (1%100)
+rmc :: RMC a
+rmc = RMC 1 [1,1,1] (1%100) 3 [(1%2,[1]),(1,[3,1]),(1%4,[1])]
 
-data DMsg = DMsg deriving Typeable
-instance Message DMsg
+data IncColN = IncColN !Int deriving Typeable
+instance Message IncColN
 
-data ModNCol = ModNCol !Int deriving Typeable
-instance Message ModNCol
+data ResizeAt = Resize :@ Direction2D deriving Typeable
+instance Message ResizeAt
 
-data DResize = ExpandL | ExpandR | ShrinkL | ShrinkR
-  deriving Typeable
-instance Message DResize
+deriving instance Eq Resize
+deriving instance Eq MirrorResize
 
-instance LayoutClass NColumns a where
-  description NColumns{..} = "NColumns " ++ show (length nums)
-  emptyLayout l _ = pure ([], Nothing)
-  pureLayout NColumns{..} r (W.Stack focus lefts rights) =
-      zip ws $ raiseFocused (length lefts) rects
-    where
-      rects = map (trans r) . concat $ zipWith3 mkRects actualWidths xdims ydimss
-      mkRects w x = map (\(y,h) -> (x, y, w, h))
 
-      xdims = scanl' (+) 0 actualWidths
-      -- in case the layout doesn't provide enough columns, we split the last one
-      actualWidths
-        | length actualNums == length widths = widths
-        | otherwise = chopLast 2 widths
+fi :: (Integral a, Num b) => a -> b
+fi = fromIntegral
 
-      ydimss = map (\n -> [(i % n, 1 % n) | i <- [0..n-1]]) actualNums
-      actualNums = distribute len nums
+takes :: Int -> [[a]] -> [[a]]
+takes _ [] = []
+takes 0 _  = []
+takes n (xs:xss)
+  | n < m     = [take n xs]
+  | otherwise = xs : takes (n-m) xss
+  where m = length xs
 
-      ws  = focus : reverse lefts ++ rights
+dropEnd :: Int -> [a] -> [a]
+dropEnd n xs = take (max 1 m) xs
+  where m = length xs - n
 
-      len = length ws
-
-  handleMessage l@NColumns{..} m = fmap join . sequence $ msum
-      [ resize <$> fromMessage m
-      , resizeCompat <$> fromMessage m
-      , modNCol <$> fromMessage m
-      , incMasterN <$> fromMessage m
-      , debug <$> fromMessage m
-      ]
-    where
-      resize = withFocusedColumn l . resize'
-
-      resizeCompat m = withFocusedColumn l $ \i -> case m of
-        Expand
-          | i == length widths - 1 -> resize' ShrinkL i
-          | otherwise -> resize' ExpandR i
-        Shrink
-          | i == 0 -> resize' ShrinkR i
-          | otherwise -> resize' ExpandL i
-
-      resize' m i = case m of
-        ExpandL -> adjustAt i (i-1) l
-        ExpandR -> adjustAt i (i+1) l
-        ShrinkL -> adjustAt (i-1) i l
-        ShrinkR -> adjustAt (i+1) i l
-
-      modNCol (ModNCol n)
-        | n < 0 = pure $ Just l {nums = consumeEnd n nums, widths = consumeEnd n widths}
-        | 0 < n = pure $ Just l {nums = nums ++ replicate n 1, widths = chopLast (n+1) widths}
-        | otherwise = pure Nothing
-
-      incMasterN (IncMasterN n) = withFocusedColumn l $ \i ->
-        Just l {nums = modifyAt i (\m -> max 1 (n+m)) nums}
-
-      debug DMsg = do
-        i <- withFocusedColumn l Just
-        Nothing <$ notify (show (l,i))
-
--- | increment at first and decrement at second index
-adjustAt :: Int -> Int -> NColumns a -> Maybe (NColumns a)
-adjustAt i j l@NColumns{..}
-  | 0 <= min i j, max i j < length widths = Just
-    l {widths = modifyAt i (+delta) $ modifyAt j (subtract delta) widths}
-  | otherwise = Nothing
-
-withFocusedColumn :: NColumns a -> (Int -> Maybe r) -> X (Maybe r)
-withFocusedColumn NColumns{..} x =
-  gets (W.stack . W.workspace . W.current . windowset) <&> \case
-    Just (W.Stack focus lefts rights) -> x $ column (length lefts) nums
-    Nothing -> Nothing
-
--- expects negative @n@
-consumeEnd :: Num n => Int -> [n] -> [n]
-consumeEnd n xs = before ++ [sum rest] where
-  (before, rest) = splitAt (length xs + n - 1) xs
-
--- | chops last element into @n@ uniform pieces
-chopLast :: Int -> [Ratio Int] -> [Ratio Int]
-chopLast n xs = init xs ++ replicate n ((1 % n) * last xs)
-
-modifyAt :: Int -> (a -> a) -> [a] -> [a]
-modifyAt i f xs = before ++ map f el ++ after
+modifyAt :: Int -> (a->a) -> [a] -> [a]
+modifyAt i f xs = as ++ map f bs ++ cs
   where
-    (before,rest) = splitAt i xs
-    (el,after) = splitAt 1 rest
+    (as, rs) = splitAt i xs
+    (bs, cs) = splitAt 1 rs
 
 -- | pull the @n@th element to the front
 raiseFocused :: Int -> [a] -> [a]
@@ -133,28 +70,110 @@ raiseFocused i xs = el ++ before ++ after
     (before,rest) = splitAt i xs
     (el,after) = splitAt 1 rest
 
--- | figure out which column the @n@th window is at
-column = go 0 where
-  go i n (w:ws)
-    | n < w     = i
-    | otherwise = go (i+1) (n-w) ws
-  go i _ _ = i
+getStack :: X (Maybe (W.Stack Window))
+getStack = gets (W.stack . W.workspace . W.current . windowset)
 
--- | distribute @n@ windows over columns which widths are given by @ms@;
--- left-over windows get appended to a new column
-distribute :: Int -> [Int] -> [Int]
-distribute n (w:ws)
-  | n <= w    = [n]
-  | otherwise = w : distribute (n-w) ws
-distribute n [] = [n]
-
--- | fit /[0;1]x[0;1]/ to a given "Rectangle"
-trans :: Rectangle -> (Ratio Int, Ratio Int, Ratio Int, Ratio Int) -> Rectangle
-trans Rectangle{..} (x,y,w,h) = Rectangle x' y' w' h'
+coords :: RMC a -> Int -> (Int,Int)
+coords RMC{..} = go grid 0
   where
-    x' = floor (x * fi rect_width)  + rect_x
-    y' = floor (y * fi rect_height) + rect_y
-    w' = floor $ w * fi rect_width
-    h' = floor $ h * fi rect_height
+    go ((_,xs):xss) i k
+      | k < m     = (i, k)
+      | otherwise = go xss (i+1) (k-m)
+      where m = length xs
+    go _ i n = (i + n `div` r, n `mod` r)
 
-    fi = fromIntegral
+    r = length rdef
+
+size :: W.Stack a -> Int
+size (W.Stack _ ls rs) = 1 + length ls + length rs
+
+numCols :: RMC a -> W.Stack b -> Int
+numCols l = succ . fst . coords l . pred . size
+
+focusedCoords :: RMC a -> W.Stack b -> (Int, Int)
+focusedCoords l = coords l . length . W.up
+
+withStack :: (W.Stack Window -> b) -> X (Maybe b)
+withStack f = fmap f <$> getStack
+
+-- | straight up the same as in XMonad.Layout.ResizableTile
+splitV, splitH :: [Rational] -> Int -> Rectangle -> [Rectangle]
+splitV [] _ r = [r]
+splitV _ n r | n < 2 = [r]
+splitV (f:fs) n (Rectangle sx sy sw sh) = Rectangle sx sy sw smallh :
+    splitV fs (n-1) (Rectangle sx (sy+fi smallh) sw (sh-smallh))
+  where smallh = min sh (floor $ fi (sh `div` fi n) * f)
+
+splitH fs n = map mirrorRect . splitV fs n . mirrorRect
+
+instance LayoutClass RMC a where
+  description _ = "ResizableMultiColumns"
+  emptyLayout _ _ = pure ([], Nothing)
+  doLayout l@RMC{..} r s@(W.Stack f ls rs) =
+      pure (wrects, if grid' /= grid then Just l {grid = grid'} else Nothing)
+    where
+      c = numCols l s
+      infGrid = grid ++ repeat (cdef, rdef)
+      cweights = take c (fst <$> infGrid)
+      crects = splitH cweights c r
+
+      rweightss = takes (size s) (snd <$> infGrid)
+      rectss = zipWith (\fs -> splitV fs (length fs)) rweightss crects
+
+      ws = f : reverse ls ++ rs
+
+      wrects = zip ws $ raiseFocused (length ls) (concat rectss)
+
+      grid' | keep < 0  = grid
+            | otherwise = take (c + keep) grid
+
+  handleMessage l@RMC{..} m' = fmap join . withStack $ \s ->
+      let
+        ij@(i,_) = focusedCoords l s
+        maxX = numCols l s
+        maxY = length $ takes (size s) (map snd grid ++ repeat rdef) !! i
+      in msum
+        [ resize ij =<< fromMessage m'
+        , resizeG Expand L R fst maxX ij =<< fromMessage m'
+        , resizeG MirrorExpand U D snd maxY ij =<< fromMessage m'
+        , incColN =<< fromMessage m'
+        , incMasterN i =<< fromMessage m'
+        ]
+    where
+      resize (i,j) m =
+        let
+          sets f k k' = Just l {grid = f k k' grid}
+          inc x = min 1 (x + delta)
+          dec x = max (1%1000) (x - delta)
+          col = sets $ \k k' ->
+              modifyAt k  (\(x, ns) -> (inc x, ns))
+            . modifyAt k' (\(x, ns) -> (dec x, ns))
+          row = sets $ \k k' ->
+            modifyAt i (modifyAt k dec . modifyAt k' inc <$>)
+        in case m of
+          Expand :@ U -> row j (j-1)
+          Expand :@ L -> col i (i-1)
+          Expand :@ D -> row j (j+1)
+          Expand :@ R -> col i (i+1)
+          Shrink :@ U -> row (j-1) j
+          Shrink :@ L -> col (i-1) i
+          Shrink :@ D -> row (j+1) j
+          Shrink :@ R -> col (i+1) i
+
+      -- non-directional expand/shrink behave different at edge
+      resizeG expand bwd fwd proj bd ij x
+        | x == expand
+        , proj ij == bd - 1 = resize ij (Shrink :@ bwd)
+        | x == expand       = resize ij (Expand :@ fwd)
+        | proj ij == 0 = resize ij (Shrink :@ fwd)
+        | otherwise    = resize ij (Expand :@ bwd)
+
+      incColN (IncColN n)
+        | n < 0 = Just l {grid = dropEnd (-n) grid}
+        | 0 < n = Just l {grid = grid ++ replicate n (cdef, rdef)}
+        | otherwise = Nothing
+
+      incMasterN i (IncMasterN n) =
+        if | n < 0 -> Just l {grid = modifyAt i (dropEnd (-n) <$>) grid}
+           | 0 < n -> Just l {grid = modifyAt i ((++replicate n 1) <$>) grid}
+           | otherwise -> Nothing
